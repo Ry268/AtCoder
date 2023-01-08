fe6fd8a2679e9a5abc22d6d6dfa859af78b78496
Ry268 <ryotaro545@gmail.com>
2023-01-08 11:32:02 +0900

    add solve

--- beginner284/B.py
+++ beginner284/B.py
@@ -1,10 +1,16 @@
+def solve(n:int, numArrays:list) -> int:
+    count = 0
+    for i in range(n):
+        if numArrays[i] % 2 != 0:
+            count += 1
+    return count
+
+
+
 t = int(input())
 
 for i in range(t):
     n = int(input())
     a = list(map(int, input().split()))
-    total = 0
-    for j in range(n):
-        if a[j] % 2 != 0:
-            total += 1
+    total = solve(n, a)
     print(total)
